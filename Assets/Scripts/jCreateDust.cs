76b5824df04e8d93f290263aa7b03172fe9514cf
Iamgroooooot <dury.ko@gmail.com>
2018-11-30 21:35:01 +0900

    NewFiles: main scene, dust, grassField, ...

---
+++ Assets/Scripts/jCreateDust.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jCreateDust : MonoBehaviour {
+    public float delay = 10f;
+    public GameObject[] dust;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(makeDust());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator makeDust()
+    {
+
+        yield return new WaitForSeconds(10f);
+    }
+}
+
