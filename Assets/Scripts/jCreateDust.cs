4fe3262ee5fd8453c8233635d6395a9a26a8f69d
Iamgroooooot <dury.ko@gmail.com>
2018-12-03 11:44:42 +0900

    NewFile - playerMaager.cs, Player의 움직임과 미세먼지 인스턴트화 설정

--- Assets/Scripts/jCreateDust.cs
+++ Assets/Scripts/jCreateDust.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class jCreateDust : MonoBehaviour {
     public float delay = 10f;
-    public GameObject[] dust;
-
+    public GameObject dust;
+    public Transform RandomPos;
+    public Rigidbody PlayerRB;
+    float Counter = 0;
+    bool NewPos = true;
 	// Use this for initialization
 	void Start () {
         StartCoroutine(makeDust());
@@ -13,13 +16,25 @@ public class jCreateDust : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (NewPos)
+        {
+            RandomPos.position = new Vector3(PlayerRB.position.x + Random.Range(-10f, 10f), PlayerRB.position.y + Random.Range(10f, 100f), PlayerRB.position.z + Random.Range(-10f, 10f));
+            NewPos = false;
+        }
+        Counter = Counter + Time.deltaTime;
+        if (Counter>1f) {
+            GameObject temp = Instantiate(dust, RandomPos, false);
+            temp.transform.SetParent(transform.parent);
+            temp.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            Counter = 0;
+            NewPos = true;
+        }
+    }
 
     IEnumerator makeDust()
     {
-
-        yield return new WaitForSeconds(10f);
+        
+        yield return new WaitForSeconds(3f);
     }
 }
 
---
+++ Assets/Scripts/jPlayerManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jPlayerManager : MonoBehaviour {
+    public GameObject mPlayer;
+    public Rigidbody mRigidBD;
+
+    void Awake()
+    {
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+        mPlayer = gameObject.GetComponent<GameObject>();
+        mRigidBD = gameObject.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        mRigidBD.velocity = new Vector3(0,0,0);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            mRigidBD.velocity = new Vector3(0,0,-20);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            mRigidBD.velocity = new Vector3(0, 0, 20);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            mRigidBD.velocity = new Vector3(20, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            mRigidBD.velocity = new Vector3(-20, 0, 0);
+        }
+        Debug.Log(mPlayer.ToString());
+    }
+}
